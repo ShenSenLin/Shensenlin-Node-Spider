39d2a104f74552e381d0d19fa879caacb1bf3169
ShenSenLin <867345686@qq.com>
2025-01-07 08:39:08 +0800

    none

--- GetNodes.py
+++ GetNodes.py
@@ -6,8 +6,25 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
 }
 
-with open('urls.txt', "r", encoding="utf-8") as f:
-    targets = f.read().split('\n')
+input_file = "urls.txt"
+output_file = "index.html"
+
+with open(input_file, "r", encoding="utf-8") as f:
+    oglt = f.read()
+
+oglt = oglt.strip()
+flag = False
+target = ''
+for i in range(len(oglt)-1):
+    if oglt[i] == oglt[i+1] == '\n' and not flag:
+        target = target + oglt[i]
+        flag = True
+    elif oglt[i] == '\n' and oglt[i+1] != '\n' and flag:
+        flag = False
+    elif not flag:
+        target += oglt[i]
+
+targets = target.split('\n')
 
 urls = ""
 for i in targets:
@@ -26,5 +43,5 @@ for i in targets:
     urls += add_ctt
     print(add_ctt)
 
-with open("result.txt", "w", encoding='utf-8') as f:
+with open(output_file, "w", encoding='utf-8') as f:
     f.write(urls)
