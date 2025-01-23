acfae3e887d2964900c2286c4403895163bd27b0
ShenSenLin <867345686@qq.com>
2025-01-23 11:32:06 +0800

    none

--- GetNodes.py
+++ GetNodes.py
@@ -1,7 +1,8 @@
 """
+某人的小脚本
 目前支持的网站：
     1、v2raya.com
-    2、freeclashnode.com
+    2、clashnode.cc
 这两个基本上就涵盖了网上能找到的绝大多数节点（非github)
 """
 
@@ -14,6 +15,7 @@ import sys
 import re
 
 
+
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
 }
@@ -21,23 +23,6 @@ headers = {
 input_file = "urls.txt"
 output_file = "README.md"
 targets = []
-
-
-def openf(file, operator, content=""):
-    if operator in ['r', 'w', 'a', 'r+', 'w+', 'a+']:
-        print('a')
-        with open(file, operator, encoding='utf-8') as f:
-            if operator in ['r', 'r+']:    content = f.read()
-            else:    
-                f.write(content)
-                print("c")
-    elif operator in ['rb', 'wb', 'ab', 'rb+', 'wb+', 'ab+']:
-        print("b")
-        with open(file, operator) as f:
-            if operator in ['rb', 'rb+']:    content = f.read()
-            else:    f.write(content)
-    else:    print("no soppused")
-    return content
 
 
 # Get time
@@ -50,7 +35,6 @@ update_time = "Update Time: " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtim
 print("Get share urls...")
 # 1、freeclashnode.com
 # https://node.clashnode.cc/uploads/2025/01/0-20250121.txt
-# winxraygithub.github.io
 for i in range(4):
     tmp = 'https://node.clashnode.cc/uploads/{0}/0{1}/{3}-{0}0{1}{2}.txt'.format(lt.tm_year, lt.tm_mon, lt.tm_mday, i)
     targets.append(tmp)
@@ -92,26 +76,23 @@ j = 0
 for i in targets:
     print(j, j / len(targets) * 100)
     j += 1
-    content = requests.get(i, headers=headers).content.decode()
 
-    # print(content)
+    content = requests.get(i, headers=headers).content.decode()
     
     if content.find(':') != -1:
         urls += content
         continue
 
+    # Padding
     pad_num = len(content) % 4
     content = content[:len(content)-pad_num]
     
     # Decoding
-    decoded = b64decode(content)
-    #detect_result = chardet.detect(decoded)
-    #coding = detect_result['encoding']
-    #print(decoded, detect_result, coding)
-    add_ctt = decoded.decode('unicode_escape')
+    add_ctt = b64decode(content).decode('unicode_escape')
 
     urls += add_ctt
-    # print(add_ctt)
 
 with open(output_file, "w", encoding='utf-8') as f:
     f.write(update_time + urls + "\n```")
+
+input('Enter to exit...')
