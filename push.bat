606a1381e367db3c598b333a1c080d27cc14b10b
ShenSenLin <867345686@qq.com>
2025-03-01 12:46:26 +0800

    none

--- push.bat
+++ push.bat
@@ -1,4 +1,12 @@
+git config --global http.proxy "socks5://127.0.0.1:10808"
+git config --global https.proxy "socks5://127.0.0.1:10808"
+
 git add --all
 git commit -m "none"
 git push -u origin main
+
+git config --global --unset http.proxy
+git config --global --unset https.proxy
+
+echo "Finished!"
 pause
